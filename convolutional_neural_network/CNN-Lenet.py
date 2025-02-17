d05cc2cd60baf865ba998fdbf2672f4e1219c662
Alpha <xtanbhjj@163.com>
2025-02-17 23:53:54 +0800

    add torch.no_grad()

--- convolutional_neural_network/CNN-Lenet.py
+++ convolutional_neural_network/CNN-Lenet.py
@@ -52,18 +52,23 @@ def train(model, optimizer, loss, device, train_iter):
         l = loss(y_hat, y)
         l.backward()
         optimizer.step()
-        metric.add(l * y.numel(), accuracy(y_hat, y), y.numel())
+
+        with torch.no_grad():
+            metric.add(l * y.numel(), accuracy(y_hat, y), y.numel())
+        
     return metric[0] / metric[2], metric[1] / metric[2]
 
 def test(model, loss, device, test_iter):
     model.eval()
     metric = Accumulator(2)
-    for x, y in test_iter:
-        x = x.to(device)
-        y = y.to(device)
-        y_hat = model(x)
-        l = loss(y_hat, y)
-        metric.add(accuracy(y_hat, y), y.numel())
+    with torch.no_grad():
+        for x, y in test_iter:
+            x = x.to(device)
+            y = y.to(device)
+            y_hat = model(x)
+            l = loss(y_hat, y)
+            metric.add(accuracy(y_hat, y), y.numel())
+
     return metric[0] / metric[1]
 
 
@@ -73,7 +78,7 @@ def main():
     trans = transforms.ToTensor()
     mnist_train = torchvision.datasets.FashionMNIST(root='../data', train=True, transform=trans, download=False)
     mnist_test = torchvision.datasets.FashionMNIST(root='../data', train=False, transform=trans, download=False)
-    train_iter = DataLoader(mnist_train, batch_size=batch_size, shuffle=True)#为两个数据集分别做一个迭代器
+    train_iter = DataLoader(mnist_train, batch_size=batch_size, shuffle=True)
     test_iter = DataLoader(mnist_test, batch_size=batch_size, shuffle=False)
     #model
     loss = nn.CrossEntropyLoss()
@@ -82,6 +87,7 @@ def main():
     #device
     device = dlf.devices()[0]
     model = model.to(device)
+    print(device)
 
     for i in range(num_epochs):
         lo, train_per = train(model, optimizer, loss, device, train_iter)
